c853411a237b054f89215eba40a06410cef4d6a2
Prava Dhulipalla <prava@students.olin.edu>
2017-03-12 21:16:08 -0400

    Turning in Word Frequency Analysis Project Toolbox

--- frequency.py
+++ frequency.py
@@ -2,6 +2,7 @@
 Project Gutenberg """
 
 import string
+from heapq import nlargest
 
 
 def get_word_list(file_name):
@@ -10,7 +11,28 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+
+    #strips out header comments
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    curr_line = 0
+    while lines[curr_line].find('POIROT EXPLAINS') == -1:
+        curr_line += 1
+    lines = lines[curr_line+1:]
+
+    #the list of all the words in the text
+    word_list = []
+
+    #strips out whitespace, punctuation, and makes lowercase
+    #makes into a list of words
+    for line in lines:
+        words = line.split()
+        for word in words:
+            word = word.strip(string.punctuation + string.whitespace)
+            word = word.lower()
+            word_list.append(word)
+
+    return word_list
 
 
 def get_top_n_words(word_list, n):
@@ -21,10 +43,27 @@ def get_top_n_words(word_list, n):
     punctuation
     n: the number of words to return
     returns: a list of n most frequently occurring words ordered from most
-    frequently to least frequentlyoccurring
+    frequently to least frequently occurring
     """
-    pass
+    word_counts = dict()
+
+    for word in word_list:
+        if word not in word_counts:
+            word_counts[word] = 1
+        else:
+            word_counts[word] += 1
+
+    topn = []
+
+    for word in nlargest(n, word_counts, key=word_counts.get):
+        topn.append(word)
+
+    return topn
 
 if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
     print(string.punctuation)
+
+    #code that calls two functions
+    wordlist = get_word_list('MysteriousAffair.txt')
+    print(get_top_n_words(wordlist, 100))
